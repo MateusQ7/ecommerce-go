5fa9c74db6ddcd50e6fe96f7ed0dfc7df3c3fa89
MateusQ7 <mateusquixada@gmail.com>
2025-08-31 14:41:46 -0300

    feat: Add More Attributes to Product Struct/Entity

--- product-service/internal/db/product_repository.go
+++ product-service/internal/db/product_repository.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/MateusQ7/ecommerce-go/product-service/internal/domain"
 )
@@ -15,28 +16,48 @@ func NewProductRepository(database *sql.DB) *ProductRepository {
 }
 
 func (r *ProductRepository) Save(product *domain.Product) error {
-	_, err := r.db.Exec("INSERT INTO products (name, price) VALUES (?, ?)", product.Name, product.Price)
-	return err
+	query := `
+		INSERT INTO products (name, description, price, stock_quantity, image_url, category_id, created_at)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)
+		RETURNING id, created_at
+	`
+
+	return r.db.QueryRow(
+		query,
+		product.Name,
+		product.Description,
+		product.Price,
+		product.StockQuantity,
+		product.ImageURL,
+		product.CategoryID,
+		time.Now(),
+	).Scan(&product.ID, &product.CreatedAt)
 }
 
 func (r *ProductRepository) FindAll() ([]domain.Product, error) {
-	rows, err := r.db.Query("SELECT id, name, price FROM products")
+	query := `SELECT id, name, description, price, stock_quantity, image_url, category_id, created_at FROM products`
 
+	rows, err := r.db.Query(query)
 	if err != nil {
 		return nil, err
 	}
-
 	defer rows.Close()
 
 	var products []domain.Product
-
 	for rows.Next() {
 		var p domain.Product
-
-		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+		if err := rows.Scan(
+			&p.ID,
+			&p.Name,
+			&p.Description,
+			&p.Price,
+			&p.StockQuantity,
+			&p.ImageURL,
+			&p.CategoryID,
+			&p.CreatedAt,
+		); err != nil {
 			return nil, err
 		}
-
 		products = append(products, p)
 	}
 
--- product-service/internal/domain/product.go
+++ product-service/internal/domain/product.go
@@ -1,7 +1,14 @@
 package domain
 
+import "time"
+
 type Product struct {
-	ID    int64   `json:"id"`
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
+	ID            int64     `json:"id"`
+	Name          string    `json:"name"`
+	Description   string    `json:"description"`
+	Price         float64   `json:"price"`
+	StockQuantity int       `json:"stock_quantity"`
+	ImageURL      string    `json:"image_url"`
+	CategoryID    int64     `json:"category_id"`
+	CreatedAt     time.Time `json:"created_at"`
 }
